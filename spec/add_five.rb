fc1708cc9d730eb679e866a78fbe9d25dc0f9cf3
Osman Khan <osman_khan1987@hotmail.com>
2023-05-10 09:26:27 +0100

    tested and passed rspec

---
+++ spec/add_five.rb
@@ -0,0 +1,14 @@
+# # File: lib/add_five.rb
+# Public: Adds 5 to a given number.
+#
+# num - The Integer to which 5 is added.
+#
+# Examples
+#
+#   add_five(3)
+#   # => 8
+#
+# Returns the Integer sum of num and 5.
+def add_five(num)
+  num + 5
+end
